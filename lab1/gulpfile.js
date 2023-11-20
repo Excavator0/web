695aa4bad82adb2864ef7efa4fbce7866ceb1779
Excavator0 <aukhadyev.subsid@gmail.com>
2023-11-20 09:54:14 +0300

    3rd commit

--- lab1/gulpfile.js
+++ lab1/gulpfile.js
@@ -1,6 +1,35 @@
-function defaultTask(cb) {
-  console.log("It works!");
-  cb();
+var gulp = require('gulp');
+var open = require('gulp-open');
+var wait = require('gulp-wait');
+
+const interval = 5000;
+const webpages = [
+	'lichess.org',
+	'chess24.com',
+	'chess.com',
+	'play.chessbase.com',
+	'chess.pro',
+];
+
+function openPage(index) {
+    if (index < webpages.length) {
+        var options = {
+            uri: webpages[index],
+            app: 'chrome',
+        };
+
+        gulp.src(__filename)
+            .pipe(open(options))
+            .pipe(wait(interval))
+            .on('end', function () {
+                openPage(index + 1);
+            });
+    }
 }
 
-exports.default = defaultTask
+gulp.task('open', function (done) {
+    openPage(0);
+    done();
+});
+
+gulp.task('default', gulp.series('open'));
